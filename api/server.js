ff3a43d77879254f3eb4bdbf4fb7bc85890dbf13
webnocode <gdfajardo6@soy.sena.edu.co>
2023-11-03 12:03:50 -0500

    first commit

---
+++ api/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser') 
+const getDocumentByTipo = require('../backend/getDocumentByTipo');
+const validateExistingUserLogin = require('../backend/validateExistingUserLogin.js')
+const createNewUser = require('../backend/createNewUser.js');
+const getTagNames = require('../backend/getTagNames.js')
+
+const app = express();
+
+app.use(cors()); 
+app.use(bodyParser.json());
+
+app.get('/api/getTagsNames', getTagNames)
+app.post('/api/documents', getDocumentByTipo);
+app.post('/api/users', createNewUser);
+app.post('/api/validateUser', validateExistingUserLogin);
+
+// Exporta una función asincrónica que recibe req y res
+module.exports = async (req, res) => {
+  // Usa tu código de Express como lo tienes
+    app(req, res, (error) => {
+        if (error) {
+        res.status(500).send(error.message);
+        }
+    });
+};
+
+
