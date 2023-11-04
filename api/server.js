c757d4776e26fba9219a65f0035baff4989cd575
webnocode <gdfajardo6@soy.sena.edu.co>
2023-11-03 22:27:16 -0500

    noche

--- api/server.js
+++ api/server.js
@@ -16,6 +16,8 @@ app.post('/api/documents', getDocumentByTipo);
 app.post('/api/users', createNewUser);
 app.post('/api/validateUser', validateExistingUserLogin);
 
+app.listen(process.env.PORT || 5000);
+
 // Exporta una función asincrónica que recibe req y res
 module.exports = async (req, res) => {
   // Usa tu código de Express como lo tienes
